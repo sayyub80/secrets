2d45809bf44dbfc6d38c5cf4cf2cf700a55bbab4
sayyub80 <mdsayyub80@gmailcom>
2023-09-11 23:28:52 +0530

    bcrypt_hash

--- app.js
+++ app.js
@@ -1,36 +1,22 @@
-//previous: Encryption-   password + key ---> ciphertext
-//Level 3--Hashing 
+//Level 4 Salting and Hashing
 /*
--->IN encryption system as long as we knew what that was then I can decode it by setting it to the same encryption key.
-And we end up being able to retrieve the original text. Now
-however, if I was to go and change this to a hash function instead, then you can see that when we try
-to decode this using the same hash function MD5, we get the error that "Decoding step is not defined
-for Hash function" because you can't really go back.
-That's the whole point of the Hash function
-and this is what will make our authentication more secure.
-So let's go ahead and implement this in our code.
+-->Now salting takes the hashing a little bit further.
+In addition to the password, we also generate a random set of characters and those characters along with
+the user's password gets combined and then put through the hash function.
+So the resulting hash is created from both the password as well as that random unique salt.
 
--->install md5
--->remove mongoose-encryption
--->require
--->set md5(req.body.password)
-
-
---> The way that hashing algorithms work you can't reverse this back into the plain text of the original password.
--->Now hashing also comes along with its own problems though because as soon as you come up with a problem
-   then some motivated hacker will come up with a solution.
-
-
-
-
+-->we're using bcrypt
+it has a concept of what's called Salt rounds. How many rounds you're going to salt your password with?
+And obviously the more rounds you do the saltier your password and also the more secure it is from hackers.
 */
 require('dotenv').config();
 const express = require("express")
-const md5=require("md5")
 const bodyParser = require("body-parser")
 const ejs = require ("ejs")
 const mongoose = require("mongoose")
 const app = express();
+const bcrypt=require("bcrypt")
+const saltRounds = 10;
 
 
 
@@ -62,30 +48,42 @@ app.get("/register",function(req,res){
 
 
 app.post("/register",function(req,res){
-    const newUser=new User ({
-        email:req.body.username,
-        password:md5(req.body.password)
-    })
-
-    
-     newUser.save().then((value)=>{
-    console.log("Successfully saved")
-    res.render("secrets");
-}).catch((err)=>{console.log("Some Error")})
   
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        
+        const newUser=new User ({
+            email:req.body.username,
+            password:hash
+        })
+    
+         
+         newUser.save().then((value)=>{
+        console.log("Successfully saved")
+        res.render("secrets");
+    }).catch((err)=>{console.log("Some Error")})
+      
+
+    });
+   
+   
+    
+
+
+
 })
 
 app.post("/login",function(req,res){
     const username = req.body.username;
-    const password = md5(req.body.password)
+    const password = req.body.password
     console.log(password)
     
       User.findOne({email:username}).then((user)=>{
-        if(user.password===password){
-            res.render("secrets")
-        }else{
-            console.log("Invalid Username or Password")
-        }
+        bcrypt.compare(password, user.password, function(err, result) {
+           if(result===true){
+             res.render("secrets")
+           }
+           
+        });
       
       }).catch((error)=>{console.log("some erroe occured")})
 
